c9a811f4a99ad95062e6e7e4a20da30a2b8c731e
BhaveshG-22 <107833962+BhaveshG-22@users.noreply.github.com>
2023-10-28 16:03:24 -0400

    typo fixed in header

--- src/components/_header.js
+++ src/components/_header.js
@@ -1,7 +1,7 @@
 function Header() {
   return (
-    <div className="h-14 bg-gray text-7xl my-5 font-semibold font-sans">
-      Drenched Clone!
+    <div className="h-14 bg-gray text-7xl my-5 font-semibold font-sans capitalize">
+      Drench Clone!
     </div>
   );
 }
